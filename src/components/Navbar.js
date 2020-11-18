a713643d2f94732aac92765635ac04b4fb5be177
ACHREKARURMILA <64479949+ACHREKARURMILA@users.noreply.github.com>
2020-11-18 20:48:41 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ function Navbar () {
     return (
         <nav className="navbar navbar-dark bg-primary justify-content-center">
         <nav className="navbar navbar-expand-lg navbar-light bg-blue">
-  <a className="navbar-brand "  href="#">LOGO</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+  <a className="navbar-brand" href="#"><img src="https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo3.png" alt="" width="200px"/> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
   <div className="collapse navbar-collapse " id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto ">
